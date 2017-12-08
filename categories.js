40ad04fb67cbb2256cbd2f8f694a2d98a93d2604
Falborski Artur <falborski.artur@gmail.com>
2017-12-08 12:43:43 +0100

    categories screen

---
+++ categories.js
@@ -0,0 +1,23 @@
+let categories = [];
+let counter = 1;
+let createBackEl = () => {
+  let aTag = document.createElement('a');
+  aTag.href = "index.html"
+  let el = document.createElement('p');
+  el.innerHTML = "Back to menu"
+  aTag.appendChild(el);
+  return aTag;
+}
+
+for (let i = 0; i < data.words.length; i++) {
+  if (!categories.includes(data.words[i].category)) {
+    categories.push(data.words[i].category);
+    let el = document.createElement('p');
+    el.style.margin = "12px 0";
+    document.getElementsByClassName('content-wrapper')[0].appendChild(el).innerHTML = `${counter}. ${data.words[i].category}`;
+    counter++;
+  }
+}
+
+document.getElementsByClassName('content-wrapper')[0].style.marginTop = "0px";
+document.getElementsByClassName('content-wrapper')[0].appendChild(createBackEl());
